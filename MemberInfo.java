f1421e73480810dac20e512642dc872eb17dfdc7
rosalindc2 <rosalindc2@nycstudents.net>
2025-05-05 22:06:38 +0000

    done

---
+++ MemberInfo.java
@@ -0,0 +1,29 @@
+public class MemberInfo {
+    private String name;
+    private int gradYear;
+    private boolean hasGoodStanding;
+
+    public MemberInfo(String name, int gradYear, boolean hasGoodStanding)
+    {
+        this.name = name;
+        this.gradYear = gradYear;
+        this.hasGoodStanding = hasGoodStanding;
+    }
+    public String toString()
+    {
+        String s = "\nName: " + name + "\n";
+        s += "Year: " + gradYear + "\n";
+        return s + "Good Standing: " + hasGoodStanding + "\n";
+    }
+
+    public boolean inGoodStanding() 
+    {
+        return hasGoodStanding;
+    }
+
+
+    public int getGradYear() 
+    {
+        return gradYear;
+    }
+}
